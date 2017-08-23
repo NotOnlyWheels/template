72e173c855ac9354c482531639ccb3106aaabcdf
jiavan <jiavan.com@gmail.com>
2017-08-24 01:04:40 +0800

    Split source html & logic code

--- template.js
+++ template.js
@@ -41,14 +41,40 @@ var each = function(data, callback) {
 };
 
 var utils = template.utils = {
-    $each: each
+    $each: each,
+    $helpers: {}
+};
+
+/**
+ * Get compile cache
+ */
+template.get = function(fileName) {
+    var cache;
+
+    if (cacheStore[fileName]) {
+        cache = cacheStore[fileName];
+    } else if (typeof document === 'object') {
+        var el = document.getElementById(fileName);
+        if (el) {
+            var source = (el.value || el.innerHTML).replace(/^\s*|\s*$/g, '');
+        }
+    }
+
+    return cache;
 };
 
 /**
  * Complie module
  */
-
 var forEach = utils.$each;
+
+// Escape html code
+function stringify(code) {
+    return '"' + code
+        .replace(/('|\\)/g, '\\$1')
+        .replace(/\r/g, '\\r')
+        .replace(/\n/g, '\\n') + '"';
+}
 
 function complier(source, options) {
 
@@ -70,6 +96,63 @@ function complier(source, options) {
     +       concat
     +   '}'; 
 
-    var headerCode = '"use strict"';
+    var headerCode = '"use strict";'
+    +   'var $utils=this,$helpers=$utils.$helpers,'
+    +   (debug ? '$line=0,' : '');
+    var mainCode = replaces[0];
+    var footerCode = 'return new String(' + replaces[3] + ')';
 
+    forEach(source.split(openTag), function(code) {
+        code = code.split(closeTag);       
+
+        var $0 = code[0];
+        var $1 = code[1];
+        
+        // Just only html snippet
+        if (code.length === 1) {
+            mainCode += html($0);
+        // Engine and html code
+        } else {
+            // mainCode += 
+        }
+    });
+
+    function html(code) {
+        line += code.split(/\n/).length - 1;
+
+        // Todo: compress
+        if (compress) {
+
+        }
+
+        if (code) {
+            code = replaces[1] + stringify(code) + replaces[2] + '\n';
+        }
+
+        return code;
+    }
+
+    function logic(code) {
+        var thisLine = line;
+
+        if (parser) {
+
+        } else if (debug) {
+            // Todo: record lien number
+        }
+
+        if (code.indexOf('=') === 0) {
+            var escapeSyntax = escape && !/^=[=#]/.test(code);
+            code = code.replace(/^=[#=]|[\s;]*$/g, '');
+
+            // Todo: escape
+            if (escapeSyntax) {
+
+            } else {
+                code = '$string(' + code + ')';
+            }
+
+            code = replaces[1] + code + replaces[2];
+        }
+    }
 }
